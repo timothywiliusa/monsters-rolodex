783d0635d0d0cbbc7cddb01e994b5203c7f13c04
Timothy <wiliusat@oregonstate.edu>
2020-09-09 16:52:38 -0700

    deploying 2

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './search-box.styles.css';
 
 export const SearchBox = ({ placeholder, handleChange }) => (
 	<input className="search" type="search" placeholder={placeholder} onChange={handleChange} />
